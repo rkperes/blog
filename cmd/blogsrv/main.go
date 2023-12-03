1cf4faaecf1b082f20524956b66f89b0ced4679f
Rafael Koch Peres <rafaelkperes@gmail.com>
2023-12-03 11:50:19 -0300

    Add htmx dependencies and scaffold

--- cmd/blogsrv/main.go
+++ cmd/blogsrv/main.go
@@ -1,7 +1,47 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+
+	htmx "github.com/donseba/go-htmx"
+	"github.com/donseba/go-htmx/middleware"
+)
+
+var _port = flag.Int("p", 3000, "port")
+
+type App struct {
+	htmx *htmx.HTMX
+}
 
 func main() {
-	fmt.Println("blogsrv")
+	flag.Parse()
+	port := *_port
+
+	// new app with htmx instance
+	app := &App{
+		htmx: htmx.New(),
+	}
+
+	mux := http.NewServeMux()
+	// wrap the htmx example middleware around the http handler
+	mux.Handle("/", middleware.MiddleWare(http.HandlerFunc(app.Home)))
+
+	err := http.ListenAndServe(fmt.Sprintf(":%d", port), mux)
+	log.Fatal(err)
+}
+
+func (a *App) Home(w http.ResponseWriter, r *http.Request) {
+	// initiate a new htmx handler
+	h := a.htmx.NewHandler(w, r)
+
+	// set the headers for the response, see docs for more options
+	h.PushURL("http://push.url")
+	h.ReTarget("#ReTarged")
+
+	// write the output like you normally do.
+	// check inspector tool in browser to see that the headers are set.
+	_, _ = h.Write([]byte("OK"))
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/rkperes/blog
 
 go 1.20
+
+require github.com/donseba/go-htmx v1.1.0
